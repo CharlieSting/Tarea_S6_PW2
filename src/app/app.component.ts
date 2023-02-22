75238f04ad20b40f8f5558a7acc4072f67c5bbcc
CharlieSting <101151950+CharlieSting@users.noreply.github.com>
2023-02-21 21:05:26 -0600

    comit 21 febrero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'Libreria Charles';
+  descripcion = 'Libreria donde podras encontrar libros de todo tipo';
+  ubicacion = 'Calle 123, Colonia 456, Ciudad 789';
+  imagenLibros= 'https://cdn.culturagenial.com/es/imagenes/novela-1984-de-george-orwell-og.jpg';
+  libro = libro1
 }
+
+export interface Ilibro{
+  titulo: string;
+  descripcion: string;
+  imagen: string;
+  editorial: string;
+}
+
+export class Libro implements Ilibro{
+  titulo: string;
+  descripcion: string;
+  imagen: string;
+  editorial: string;
+  constructor(titulo: string, descripcion: string, imagen: string, editorial: string){
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+    this.editorial = editorial;
+  }
+}
+
+// crear libros
+let libro1 = new Libro('1984', 'Novela de ciencia ficción', 'https://cdn.culturagenial.com/es/imagenes/novela-1984-de-george-orwell-og.jpg', 'Editorial 1');
+
+
+
---
+++ src/app/interfaces/ILibro.ts
@@ -0,0 +1,6 @@
+export interface ILibro{
+  titulo: string;
+  descripcion: string;
+  imagen: string;
+  editorial: string;
+}
